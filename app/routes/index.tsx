d34c74759ab87f419e2ee0fe818eff8580167977
anuuyu <yusrandante@gmail.com>
2022-09-29 01:08:34 +0800

    update keterangan

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,20 @@
 export default function Index() {
   return (
     <div>
+      <div className="md:container mx-auto">
       <h1 className="text-3xl font-bold underline flex justify-center">
       myu
     </h1>
-      
+      <p>
+        hi, saya seorang freelancer dibidang pengembangan website (web development) dan desain grafis.
+      </p>
+      <p>
+        saya familiar dengan html, css, javascript, php, wordpress, JAMstack dan beberapa alat yang sering digunakan dalam proses pengembangan website.
+      </p>
+      <p>
+        dalam hal desain grafis, saya terbiasa dengan alat seperti illustrator, photoshop, coreldraw, gimp, inkscape. beberapa waktu terakhir ini saya menggunakan inkscape sebagai alat utama saya.
+      </p>
+      </div>
     </div>
   );
 }
