ae9372911097d8e1d4d3106874f7b9c941911cb1
anuuyu <yusrandante@gmail.com>
2022-10-21 09:41:08 +0800

    :rocket:

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
         saya familiar dengan html, css, javascript, php, wordpress, dan beberapa CMS lainnya. akhir akhir ini saya sedang mempelajari tentang reactjs, JAMstack dan beberapa alat yang sering digunakan dalam proses pengembangan website.
       </p>
       <p>
-        dalam hal desain grafis, saya terbiasa dengan alat seperti illustrator, photoshop, coreldraw, gimp, inkscape. beberapa waktu terakhir ini saya menggunakan inkscape sebagai alat utama saya.
+        dalam hal desain grafis, saya tertarik mempelajari tentang UI design dan juga logo design. saya familiar dengan alat desain grafis seperti illustrator, photoshop, coreldraw, gimp, inkscape. beberapa waktu terakhir ini saya menggunakan inkscape sebagai alat utama saya.
       </p>
       </div>
     </div>
